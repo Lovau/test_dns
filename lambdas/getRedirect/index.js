8741b369cbfa478d3ea26d9304a095f806a534b1
Loic <loic.vauvrecy@gmail.com>
2022-01-30 20:33:24 +0100

    Update redirect lambda to retrieve AEM folder, language, brand

--- lambdas/getRedirect/index.js
+++ lambdas/getRedirect/index.js
@@ -2,23 +2,22 @@ exports.handler = async (event) => {
     var response;
     var URL = "";
     var responseHeaders = {
-            "Access-Control-Allow-Headers" : "Content-Type",
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
     };
-    
+
     if (event.queryStringParameters && event.queryStringParameters.URL) {
         console.log("Received name: " + event.queryStringParameters.URL);
         URL = event.queryStringParameters.URL;
     } else {
         response = {
-          statusCode: 400,
-          headers: responseHeaders,
-          body: JSON.stringify({
-              err:"URL parameter is missing",
-              event: event
-          })
-              
+            statusCode: 400,
+            headers: responseHeaders,
+            body: JSON.stringify({
+                err: "URL parameter is missing",
+                event: event,
+            }),
         };
         return response;
     }
@@ -28,25 +27,26 @@ exports.handler = async (event) => {
         if (!response.redirect) {
             throw Error("No redirection found");
         }
-        
+
         response = {
             statusCode: 200,
             headers: responseHeaders,
-            body: JSON.stringify(response)
+            body: JSON.stringify(response),
         };
-    } catch(err) {
+    } catch (err) {
         response = {
             statusCode: 404,
             headers: responseHeaders,
-            body: JSON.stringify(err)
-        }; 
+            body: JSON.stringify(err),
+        };
     }
-    
+
     return response;
 };
 
 async function getRedirect(fullURL) {
-    const request = require('request');
+    const request = require("request");
+    const { parse } = require("node-html-parser");
 
     return new Promise((resolve, reject) => {
         const options = {
@@ -55,26 +55,40 @@ async function getRedirect(fullURL) {
             url: fullURL,
             method: "GET",
         };
-        
-        var r = request(options, function (err, res, body) {
 
+        var r = request(options, function (err, res, body) {
             if (err) {
                 reject(err);
-            }
-            else {
+            } else {
                 // console.log(r.uri.href);
                 // console.log(res.request.uri.href);
                 // console.log(this.uri.href);
-        
-                var isRolex = function(body) {
+
+                const root = parse(body);
+                const html = root.querySelector("html");
+                const aemFolder = html.getAttribute("data-root");
+                const aemLanguage = html.getAttribute("data-languagenode");
+                const aemBrand = html.getAttribute("data-brand");
+
+                var isRolex = function (body) {
                     return (body.match(/rolex/g) || []).length > 1;
-                }
+                };
                 var response = {
                     redirect: res.request.uri.href,
-                    isRolex: isRolex(body)
-                }
+                    isRolex: isRolex(body),
+                    AEM: {
+                        folder: aemFolder,
+                        language: aemLanguage,
+                        brand: aemBrand,
+                    },
+                };
                 resolve(response);
             }
         });
     });
 }
+// local test
+// (async () => {
+//     var res = await getRedirect("https://qrt.aptaclub.it/0539152247310228NQ3MNSX35");
+//     console.log("res", res);
+// })();
